510b07482634f617a9bc0e73af8af10443d2595c
Kushan Fernando <Kushan.Fernando@trustwave.com>
2024-03-29 09:57:29 +1300

    new pyproject and setup.py

---
+++ custom_components/remootio/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aioremootio',
+    version='2.0',
+    author='TritonNet',
+    author_email='info@kushan.me',
+    description='Asynchronous Python client for interfacing with Remootio devices.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TritonNet/hass-remootio',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
